c16422415e2f16c3174c83a72a1b6f3e90c5651e
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-11-01 05:47:51 -0300

    City - All cities sorted by name

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.bds02.services;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+
+@Service
+public class CityService {
+
+	@Autowired
+	private CityRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<CityDTO> findAll() {
+		List<City> City = repository.findAll();
+		List<CityDTO> listDTO = City.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+		Collections.sort(listDTO, Comparator.comparing(CityDTO::getName));
+		return listDTO;
+	}
+}
